336cf3517663fd10e44ef8cf0429387fc3191db6
pritchard2751 <a.pritchard1@live.co.uk>
2020-05-26 19:45:20 +0100

    Add controller class not found error handling TODO

--- index.php
+++ index.php
@@ -8,8 +8,16 @@ require_once 'core/config/config.php';
 require_once 'core/config/routes.php';
 
 $urlParser = new \Core\helpers\URLParser($_GET);
+$params = $urlParser->getAddtionalURLParams();
 $controllerLoader = new \Core\helpers\Loader($routes, $urlParser);
-$controllerLoader->createControllerInstance();
+
+try {
+    $controllerLoader->createControllerInstance($params);
+} catch (Error $e) {
+    echo $e->getMessage();
+    die('TODO');
+}
+
 $controllerInstance = $controllerLoader->getControllerInstance();
 $controllerInstance->executeAction();
 
